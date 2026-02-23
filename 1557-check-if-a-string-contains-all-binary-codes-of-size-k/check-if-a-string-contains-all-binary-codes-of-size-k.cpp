db8ff67f0ee08a26d0298a6217f361476cfbb6a6
rishi-sanjay <rishisanjay44@gmail.com>
2026-02-23 17:34:40 +0530

    Time: 1071 ms (11.95%) | Memory: 158 MB (16.48%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        set<int> se;
-        int sz = s.size();
-        if (sz < k)
-            return false;
-        int p = 1, ans = 0;
-        for (int i = k - 1; i >= 0; i--) {
-            if (s[i] == '1')
-                ans = ans + p;
-            p *= 2;
+        if(s.size()<k) return false;
+        set<string> se;
+        for(int i=0;i<s.size()-k+1;i++){
+            string ne=s.substr(i,k);
+            se.insert(ne);
         }
-        
-        se.insert(ans);
-        for (int i = k; i < sz; i++) {
-            ans = ans * 2;
-            if (s[i - k] == '1')
-                ans = ans - p;
-            if (s[i] == '1')
-                ans += 1;
-            if (se.find(ans) == se.end())
-                se.insert(ans);
-        }
-        if (k == 1)
-            return se.size() == 2;
-        int range = 1 << k;
-        return se.size() == range;
+        return se.size()==(1<<k);
     }
 };
