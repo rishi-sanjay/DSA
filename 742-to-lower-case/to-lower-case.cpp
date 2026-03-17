b25b279bf23b5ae04ba43e1cc3232a464c57112c
rishi-sanjay <rishisanjay44@gmail.com>
2026-03-17 23:31:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (14.91%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string ans = "";
+        for (char x : s) {
+            ans += tolower(x);
+        }
+        return ans;
+    }
+};
