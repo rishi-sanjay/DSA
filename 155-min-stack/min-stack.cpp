9c60949aabee97dda840910e0dca64c87f182b8f
rishi-sanjay <rishisanjay44@gmail.com>
2026-03-28 23:52:15 +0530

    Time: 8 ms (11.95%) | Memory: 23.7 MB (21.73%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -2,42 +2,41 @@ class MinStack {
 public:
     stack<long long> s;
     long long ans;
-    MinStack() { ans = INT_MAX; }
-
+    MinStack() {
+        ans=INT_MAX;
+    }
+    
     void push(int val) {
         long long x=val;
-        if (s.empty()) {
-            ans = x;
+        if(s.empty()){
             s.push(x);
-        } else if (ans > x) {
-            s.push(2 * x - ans);
-           ans=x;
-        } else
-            s.push(x);
+            ans=x;
+        }else if(x<ans){
+            s.push(2*x-ans);
+            ans=x;
+        }else
+           s.push(x);
     }
-
+    
     void pop() {
-        if (s.empty())
-            return;
-        if (s.top() < ans) {
-            ans = 2 * ans - s.top();
+        if(s.empty()) return;
+        if(s.top() < ans){
+            ans=2*ans- s.top();
         }
         s.pop();
     }
-
+    
     int top() {
-        if (s.empty())
-            return -1;
-        if (s.top() > ans) {
-            return (int)s.top();
-        } else
-            return (int)ans;
+        if(s.empty()) return -1;
+        if(s.top()<ans)
+           return (int) ans;
+        return (int) s.top();
     }
 
+    
     int getMin() {
-        if (s.empty())
-            return -1;
-        return (int)ans;
+        if(s.empty()) return -1;
+        return (int ) ans;
     }
 };
 
