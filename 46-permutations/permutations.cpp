ea0231f0ff611ae3813c2159a8f1c99fed2fcca2
rishi-sanjay <rishisanjay44@gmail.com>
2026-04-17 23:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.08%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+void helper(vector<vector<int>> &ans,vector<int>&nums, int idx){
+    if(idx == nums.size()){
+        ans.push_back({nums});
+        return;
+    }
+
+    for(int i = idx; i<nums.size(); i++){
+        swap(nums[i], nums[idx]);
+        helper(ans, nums, idx+1);
+        swap(nums[i], nums[idx]);
+    }
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+    
+        helper(ans,  nums, 0);
+        return ans;
+    }
+};
