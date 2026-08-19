b5df85a665038bc35130b1a8f399b93ac980dc05
rishi-sanjay <rishisanjay44@gmail.com>
2026-08-20 00:49:44 +0530

    Time: 8 ms (7.76%) | Memory: 71.2 MB (5.88%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -6,18 +6,19 @@ public:
         int ans = 0, next;
         for (int i = 1; i < sz; i++) {
             if (ratings[i - 1] < ratings[i])
-                st[i]=st[i-1]+1;
+                st[i] = st[i - 1] + 1;
         }
         int curr = st[sz - 1];
         ans = curr;
-        next=curr;
+        next = curr;
         for (int i = sz - 2; i >= 0; i--) {
-        
-            if (ratings[i] > ratings[i+1]) {
-                st[i]=max(st[i+1]+1,st[i]);
-            }
 
-            ans += st[i];
+            if (ratings[i] > ratings[i + 1]) {
+                next = curr + 1;
+                curr = next;
+            } else
+                curr = 1;
+            ans += max(curr, st[i]);
         }
 
         return ans;
