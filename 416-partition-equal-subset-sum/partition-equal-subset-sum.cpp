ae40d90b3979556a63d0cb852946c21d804fd8c6
rishi-sanjay <rishisanjay44@gmail.com>
2026-09-05 01:04:54 +0530

    Time: 167 ms (26.73%) | Memory: 205.6 MB (6.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    bool call(int tar, int sum, vector<int>& nums, int idx, vector<vector<int>>&dp) {
-        if (sum == tar)
+    bool call(int tar, vector<int>& nums, int idx, vector<vector<int>>& dp) {
+        if (tar == 0)
             return true;
-        if(dp[idx][sum]!=-1) return dp[idx][sum];
-        if (idx == 0 || tar > sum)
-            return false;
-        bool np = call(tar, sum, nums, idx - 1,dp);
+        if (dp[idx][tar] != -1)
+            return dp[idx][tar];
+        if (idx == 0)
+            return nums[0] == tar;
+        bool np = call(tar, nums, idx - 1, dp);
         bool pi = false;
-        if (tar + nums[idx] <= sum - nums[idx])
-            pi = call(tar + nums[idx], sum - nums[idx], nums, idx - 1,dp);
-        return dp[idx][sum]=pi || np;
+        if (tar - nums[idx] > 0)
+            pi = call(tar - nums[idx], nums, idx - 1, dp);
+        return dp[idx][tar] = pi || np;
     }
     bool canPartition(vector<int>& nums) {
-        
+
         int i = 0, sum = 0;
-        for (int val : nums){
+        for (int val : nums) {
             sum += val;
-            i+=1;
+            i += 1;
         }
-        vector<vector<int>>dp(i+1,vector<int>(sum+1,-1));
-        return call(0, sum, nums, i-1,dp);
+        if (sum % 2)
+            return false;
+        vector<vector<int>> dp(i + 1, vector<int>(sum + 1, -1));
+        return call(sum / 2, nums, i - 1, dp);
     }
 };
