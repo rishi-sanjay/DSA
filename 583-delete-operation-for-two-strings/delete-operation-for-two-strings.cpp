379120925e9a734bb0186af58eb481cc044699af
rishi-sanjay <rishisanjay44@gmail.com>
2026-09-08 13:22:45 +0530

    Time: 15 ms (34.36%) | Memory: 16.5 MB (76.67%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int call(string &s,string&t,int i,int j, vector<vector<int>>&dp){
+    if(i<0 || j<0) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==t[j]) 
+       return dp[i][j]=1+ call(s,t,i-1,j-1,dp);
+    return dp[i][j]=0 + max(call(s,t,i-1,j,dp),call(s,t,i,j-1,dp));
+}
+    int longestCommonSubsequence(string &text1, string &text2) {
+        int m=text1.size();
+        int n=text2.size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return call(text1,text2,m-1,n-1,dp);
+    }
+
+    int minDistance(string word1, string word2) {
+        int sz=longestCommonSubsequence(word1,word2);
+        return (word1.size()-sz+word2.size()-sz);
+    }
+};
