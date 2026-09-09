8e80e5b476db39aadad79e320d66d49c2c9694b6
rishi-sanjay <rishisanjay44@gmail.com>
2026-09-10 00:10:47 +0530

    Time: 11 ms (81.33%) | Memory: 9.2 MB (78.56%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int f(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        if (j < 0)
-            return 1;
-        if (i < 0)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (s[i] == t[j])
-            return dp[i][j] = f(i - 1, j - 1, s, t, dp) + f(i - 1, j, s, t, dp);
-        else
-            return dp[i][j] = f(i - 1, j, s, t, dp);
-    }
-    int numDistinct(string s, string t) {
+      int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        vector<vector<double>> dp(m + 1, vector<double>(n + 1, 0));
-        for (int i = 0; i <= m; i++)
-            dp[i][0] = 1;
+        vector<double>prev(n+1,0),curr(n+1,0);
+        prev[0]=curr[0]=1;
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s[i - 1] == t[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1] +dp[i - 1][j];
+                    curr[j] = prev[j - 1] + prev[j];
                 else
-                    dp[i][j] = dp[i - 1][j];
+                    curr[j] = prev[j];
             }
+            prev=curr;
         }
-        return (int)dp[m][n];
+        return (int)prev[n];
     }
 };
